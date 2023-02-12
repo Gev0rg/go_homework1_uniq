5db33ebd485f7020986a5606554f0a43c403a9a9
Gevork Gabrielyan <gevorkgabrielan@gmail.com>
2023-02-12 20:53:08 +0300

    Added handling -i flag

--- uniq.go
+++ uniq.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,9 +14,9 @@ func main() {
 	var numOccurrencesStrings bool
 	flag.BoolVar(&numOccurrencesStrings, "c", false, "print the number of string occurrences")
 	var onlyOccurrencesStrings bool
-	flag.BoolVar(&onlyOccurrencesStrings, "d", false, "print only occurrences strings")
+	flag.BoolVar(&onlyOccurrencesStrings, "d", false, "print only occurrences inputStrings")
 	var onlyNotOccurrencesStrings bool
-	flag.BoolVar(&onlyNotOccurrencesStrings, "u", false, "print only not occurrences strings")
+	flag.BoolVar(&onlyNotOccurrencesStrings, "u", false, "print only not occurrences inputStrings")
 	var numFields int
 	flag.IntVar(&numFields, "f", 0, "number of fields to skip from the string")
 	var numChars int
@@ -23,13 +24,13 @@ func main() {
 	var caseInsensitive bool
 	flag.BoolVar(&caseInsensitive, "i", false, "case-insensitive")
 
+	// Check for having the flags
 	flag.Parse()
 
-
-	// Check for having the file
+	// Check for having the input file
 	var in io.Reader
-	if filename := flag.Arg(0); filename != "" {
-		f, err := os.Open(filename)
+	if inputFile := flag.Arg(0); inputFile != "" {
+		f, err := os.Open(inputFile)
 		if err != nil {
 			fmt.Println("error opening file: err:", err)
 			os.Exit(1)
@@ -42,21 +43,51 @@ func main() {
 	}
 
 	// Create the scanner
-	buf := bufio.NewScanner(in)
+	inBuf := bufio.NewScanner(in)
 
-	// Create slice to store the lines
-	strings := []string{}
+	// Create slice to store the input lines
+	inputStrings := []string{}
 
-	for ; buf.Scan(); {
-		strings = append(strings, buf.Text()) 
+	for ; inBuf.Scan(); {
+		inputStrings = append(inputStrings, inBuf.Text()) 
 	}
 
 	// Check for errors from the scanner
-	if err := buf.Err(); err != nil {
+	if err := inBuf.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "error reading: err:", err)
 	}
 
-	for _, i := range(strings) {
-		fmt.Println(i)
+	// Create slice to store the result lines
+	outputStrings := []string{}
+
+	// Check for having the output file
+	var out io.Writer
+	if outputFile := flag.Arg(1); outputFile != "" {
+		f, err := os.Create(outputFile)
+		if err != nil {
+			fmt.Println("error opening file: err:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		out = f
+	} else {
+		out = os.Stdout
+    }
+
+	if caseInsensitive {
+		var prev string
+		for _, s := range inputStrings {
+			if strings.ToLower(s) == strings.ToLower(prev) {
+                continue
+            }
+			outputStrings = append(outputStrings, prev)
+            prev = s
+		}
+		outputStrings = append(outputStrings, prev)
+	}
+
+	for _, s := range outputStrings[1:] {
+		fmt.Fprintln(out, s)
 	}
 }
