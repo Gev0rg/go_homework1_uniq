59479110ff084263e9e3c73b2b532e543560c2f6
Gevork Gabrielyan <gevorkgabrielan@gmail.com>
2023-02-15 18:45:49 +0300

    Refactoring

--- uniq.go
+++ uniq.go
@@ -10,7 +10,32 @@ import (
 	"strings"
 )
 
-func getOutputSlice(inputStrings []string, compareStrings []string) ([]string, []int) {
+// All Flags
+var numOccurrencesStrings bool
+var onlyOccurrencesStrings bool
+var onlyNotOccurrencesStrings bool
+var numFields int
+var numChars int
+var caseInsensitive bool
+
+// Check all flags
+func checkFlags() error {
+	// Init Flags
+	flag.BoolVar(&numOccurrencesStrings, "c", false, "print the number of string occurrences")
+	flag.BoolVar(&onlyOccurrencesStrings, "d", false, "print only occurrences inputStrings")
+	flag.BoolVar(&onlyNotOccurrencesStrings, "u", false, "print only not occurrences inputStrings")
+	flag.IntVar(&numFields, "f", 0, "number of fields to skip from the string")
+	flag.IntVar(&numChars, "s", 0, "number of characters to skip from the string")
+	flag.BoolVar(&caseInsensitive, "i", false, "case-insensitive")
+
+	// Check for having the flags
+	flag.Parse()
+
+	return nil
+}
+
+// Get unique strings from inputStrings
+func getUniqSlice (inputStrings []string, compareStrings []string) ([]string, []int) {
 	outputStrings := []string{}
 	numStrings := []int{}
 	numStrings = append(numStrings, 0)
@@ -33,54 +58,8 @@ func getOutputSlice(inputStrings []string, compareStrings []string) ([]string, [
 	return outputStrings, numStrings
 }
 
-func main() {
-	// All Flags
-	var numOccurrencesStrings bool
-	flag.BoolVar(&numOccurrencesStrings, "c", false, "print the number of string occurrences")
-	var onlyOccurrencesStrings bool
-	flag.BoolVar(&onlyOccurrencesStrings, "d", false, "print only occurrences inputStrings")
-	var onlyNotOccurrencesStrings bool
-	flag.BoolVar(&onlyNotOccurrencesStrings, "u", false, "print only not occurrences inputStrings")
-	var numFields int
-	flag.IntVar(&numFields, "f", 0, "number of fields to skip from the string")
-	var numChars int
-	flag.IntVar(&numChars, "s", 0, "number of characters to skip from the string")
-	var caseInsensitive bool
-	flag.BoolVar(&caseInsensitive, "i", false, "case-insensitive")
-
-	// Check for having the flags
-	flag.Parse()
-
-	// Check for having the input file
-	var in io.Reader
-	if inputFile := flag.Arg(0); inputFile != "" {
-		f, err := os.Open(inputFile)
-		if err != nil {
-			fmt.Println("error opening file: err:", err)
-			os.Exit(1)
-		}
-		defer f.Close()
-
-		in = f
-	} else {
-		in = os.Stdin
-	}
-
-	// Create the scanner
-	inBuf := bufio.NewScanner(in)
-
-	// Create slice to store the input lines
-	inputStrings := []string{}
-
-	for ; inBuf.Scan(); {
-		inputStrings = append(inputStrings, inBuf.Text()) 
-	}
-
-	// Check for errors from the scanner
-	if err := inBuf.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "error reading: err:", err)
-	}
-
+// Get output strings from inputStrings
+func getOutputSlice(inputStrings[]string) []string {
 	// Create slice to store the lines for compare after flag parsing
 	compareStrings := make([]string, len(inputStrings))
 	copy(compareStrings, inputStrings)
@@ -108,13 +87,49 @@ func main() {
     }
 
 	// Create slice to store the result lines
-	outputStrings, numStrings := getOutputSlice(inputStrings, compareStrings)
+	compareStrings, numStrings := getUniqSlice(inputStrings, compareStrings)
 
 	if numOccurrencesStrings {
-		for i, s := range outputStrings {
-			outputStrings[i] = strconv.Itoa(numStrings[i]) + " " + s
+		for i, s := range compareStrings {
+			compareStrings[i] = strconv.Itoa(numStrings[i]) + " " + s
 		}
     }
+
+	outputStrings := []string{}
+    if onlyOccurrencesStrings && !onlyNotOccurrencesStrings {
+		for i, s := range outputStrings {
+			if numStrings[i] > 1 {
+				outputStrings = append(outputStrings, s)
+			}
+		}
+	} else if !onlyOccurrencesStrings && onlyNotOccurrencesStrings {
+		for i, s := range outputStrings {
+			if numStrings[i] == 1 {
+				outputStrings = append(outputStrings, s)
+			}
+		}		
+	} else {
+		outputStrings = compareStrings
+	}
+
+	return outputStrings
+}
+
+func main() {
+	// Check for having the input file
+	var in io.Reader
+	if inputFile := flag.Arg(0); inputFile != "" {
+		f, err := os.Open(inputFile)
+		if err != nil {
+			fmt.Println("error opening file: err:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		in = f
+	} else {
+		in = os.Stdin
+	}
 
 	// Check for having the output file
 	var out io.Writer
@@ -131,21 +146,25 @@ func main() {
 		out = os.Stdout
 	}
 
-    if onlyOccurrencesStrings && !onlyNotOccurrencesStrings {
-		for i, s := range outputStrings {
-			if numStrings[i] > 1 {
-				fmt.Fprintln(out, s)
-			}
-		}
-	} else if !onlyOccurrencesStrings && onlyNotOccurrencesStrings {
-		for i, s := range outputStrings {
-			if numStrings[i] == 1 {
-				fmt.Fprintln(out, s)
-			}
-		}		
-	} else {
-		for _, s := range outputStrings {
-			fmt.Fprintln(out, s)
-		}
+	// Check for having the flags
+	checkFlags()
+
+	// Create the scanner
+	inBuf := bufio.NewScanner(in)
+	// Create slice to store the input lines
+	inputStrings := []string{}
+	for ; inBuf.Scan(); {
+		inputStrings = append(inputStrings, inBuf.Text()) 
+	}
+	// Check for errors from the scanner
+	if err := inBuf.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error reading: err:", err)
+	}
+
+	// Get the output strings
+	outputStrings := getOutputSlice(inputStrings)
+
+	for _, s := range outputStrings {
+		fmt.Fprintln(out, s)
 	}
 }
